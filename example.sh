ce0244032e37856cd0acacee8db851215cf48764
Ashwith Kumar <90125583+Ashwithkumar@users.noreply.github.com>
2023-03-16 20:01:06 +0530

    shell file

---
+++ example.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+url="https://www.example.com"
+
+status_code=$(curl --write-out "%{http_code}\n" --silent --output /dev/null "$url")
+
+if [[ $status_code -eq 200 ]]; then
+  echo "$url is up and running!"
+else
+  echo "$url returned an HTTP status code of $status_code"
+fi
